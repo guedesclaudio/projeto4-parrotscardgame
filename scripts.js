87afa506f39ed8d86801e92c1fad107d87f02ef2
guedesclaudio <cgsarmentosilva@gmail.com>
2022-06-15 00:09:01 -0300

    Quarto commit

--- scripts.js
+++ scripts.js
@@ -1,38 +1,58 @@
 let numberCards = prompt("Com quantas cartas deseja jogar?")
+numberCards = Number(numberCards) 
 
-const myArray = ['bobrossparrot', 'bobrossparrot',
-                'explodyparrot', 'explodyparrot',
-                'metalparrot','metalparrot',
-                'revertitparrot', 'revertitparrot',
-                'tripletsparrot', 'tripletsparrot',
-                'unicornparrot', 'unicornparrot',
-                'fiestaparrot', 'fiestaparrot']
+const myArray = ['bobrossparrot', 'explodyparrot', 'metalparrot','revertitparrot', 'tripletsparrot','unicornparrot','fiestaparrot']
 
 let plays = 0
+let cardArray = []
+let position = 0
 
-
-if (numberCards <= 14 && numberCards % 2 == 0) {
-    for (let i = 0; i < numberCards; i++) {
+if (numberCards <= 14 && numberCards > 2 && numberCards % 2 == 0) {
+    for (let i = 0; i < numberCards/2; i++) {
         createElement()
     }
 }
 else {
-    while (numberCards > 14 || numberCards % 2 != 0) {
-        numberCards = prompt("Com quantas cartas deseja jogar? Máximo de 14 e somente números pares")
+    while (numberCards > 14 || numberCards % 2 != 0 || numberCards < 3) {
+        numberCards = prompt("Com quantas cartas deseja jogar? Máximo de 14 e minímo de 4, somente números pares")
     }
-    for (let i = 0; i < numberCards; i++) {
+    for (let i = 0; i < numberCards/2; i++) {
         createElement()
     }
 }
 
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
 function createElement() {
+    
     const newCard = `
     <div class="card" data-identifier="card" onclick="gameCards(this)">
-        <img src="img/front 1.png" alt="">
+        <div class="front-card"><img src="img/front 1.png" alt=""></div>
+        <div class="back-card hidden"><img src="img/${myArray[position]}.gif" alt=""></div>
     </div>
     `
-    const dadDiv = document.querySelector(".container")
-    dadDiv.innerHTML += newCard
+    const newCard2 = `
+    <div class="card" data-identifier="card" onclick="gameCards(this)">
+        <div class="front-card"><img src="img/front 1.png" alt=""></div>
+        <div class="back-card hidden"><img src="img/${myArray[position]}.gif" alt=""></div>
+    </div>
+    `
+    console.log(myArray[position])
+    cardArray.push(newCard)
+    cardArray.push(newCard2)
+    position += 1
+
+    if (cardArray.length == numberCards) {
+        cardArray = cardArray.sort(comparador)
+
+        for (let x = 0; x < numberCards; x++) {
+            const dadDiv = document.querySelector(".container")
+            dadDiv.innerHTML += cardArray[x]
+        }
+    }
+    
 }
 
 function gameCards(element) {
@@ -42,17 +62,11 @@ function gameCards(element) {
     if (clickedElement == false) {
 
         element.classList.add("clicked")
-
-        let random = myArray[Math.round(Math.random()*13)]
-
         plays += 1
 
-        element.querySelector("img").src = `img/${random}.gif`
-    }
+        element.querySelector(".front-card").classList.add("hidden")
+        element.querySelector(".back-card").classList.remove("hidden")
 
-    /*else {
-        element.querySelector("img").src = "img/front 1.png"
-        console.log('passando else')
-    }*/
+    }
 
 }
